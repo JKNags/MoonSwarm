23f78116a46b1ab96b01e9485d3dd9ec6ca7c9a3
JKNags <johnknagel@gmail.com>
2019-11-22 15:02:51 -0500

    implemented reflection, no randomness on reversal

--- moon_swarm.py
+++ moon_swarm.py
@@ -6,18 +6,18 @@ import numpy as np
 import math
 
 # Global Variables
-min_blob_size = 20   # Distance for particle to reflect within blob
+min_blob_size = 8   # Distance for particle to reflect within blob
 search_color = (255, 0, 0)
-color_threshold_r = 225  # Min
-color_threshold_g = 80   # Max
-color_threshold_b = 80   # Max
+color_threshold_r = 200  # Min
+color_threshold_g = 100   # Max
+color_threshold_b = 100   # Max
 
 def is_in_color(color):
 	return color[0] > color_threshold_r and color[1] < color_threshold_g and color[2] < color_threshold_b
 
 # Read Image
-img = mpimg.imread('grail_gravity_map_moon.jpg') 
-#img = mpimg.imread('test1.jpg')[:,:,:3]
+#img = mpimg.imread('grail_gravity_map_moon_sm.jpg')[::-1,:]
+img = mpimg.imread('test/test3.jpg')[::-1,:]#[:,:,:3]
 
 # Figure
 fig, ax = plt.subplots()
@@ -31,18 +31,25 @@ plt.ylim(0,img_y)   # set y limits of plot
 #print ("Y=", img.shape[0])
 
 # Output Images 
-ax.imshow(img)   # show gravity map on plot
+ax.imshow(img, origin='lower')    # show gravity map on plot
+
+states = {0:"Travelling", 1:"Inside large blob", 2:"Edge Tracing"}
 
 # Create Swarm
 swarm_size = 1
-swarm = np.zeros(swarm_size, dtype=[('position',  int, 2),   # Position (x,y)
-                                   ('velocity',  int, 2),    # Velocity (dx,dy)
-                                   ('color',     float, 3),  # Color (r,g,b)
-                                   ('in_color_count', int, 1)])  
+swarm = np.zeros(swarm_size, dtype=[('position',       int, 2),    # Position (x,y)
+                                   ('velocity',        int, 2),    # Velocity (dx,dy)
+                                   ('color',           float, 3),  # Color (r,g,b)
+                                   ('state',   		   int, 1),    # Is currently tracing edge
+                                   ('last_move',       int, 1),    # Recently moved neighboring index
+                                   ('backtrack_point', int, 2),    # 
+                                   ('in_color_count',  int, 1)])   # Num iter in color
 
 # return random int, not 0
+# n - item count, d - dimentions per item
 def get_rand_velocity(n, d):
-	return np.random.choice([-5,-4,-3,-2,-1,1,2,4,5], (n, d))
+	#return np.random.choice([-5,-4,-3,-2,-1,1,2,4,5], (n, d))
+	return np.random.choice([-3,-2,-1,1,2,3], (n, d))
 
 scale = 10
 particle_size = 25 * scale  # Set particle size
@@ -55,10 +62,15 @@ swarm['velocity'] = get_rand_velocity(swarm_size, 2)   # Set velocity components
 swarm['color'] = [tuple(img[particle['position'][1]][particle['position'][0]]) for particle in swarm]
 
 
-
-swarm['position'][:, 0] = 147
-swarm['position'][:, 1] = 344
-
+"""
+THIS IS FOR TESTING, REMOVE LATER
+"""
+swarm['position'][:,0] = 1 #180 #182
+swarm['position'][:,1] = 1 #88 #386
+#swarm['velocity'] = (1,1)
+"""
+THIS IS FOR TESTING, REMOVE LATER
+"""
 
 
 # Create initial scatter plot
@@ -80,114 +92,130 @@ def set_markers():
 
 set_markers() # set plot markers as triangles with rotation wrt velocity
 
+def translate_neighbor_to_velocity(idx):
+	_vy = 0
+	_vx = 0
+	if (idx in (6,7,0)):
+		_vy = 1
+	if (idx in (5,1)):
+		_vy = 0
+	if (idx in (4,3,2)):
+		_vy = -1
+	if (idx in (0,1,2)):
+		_vx = 1
+	if (idx in (7,3)):
+		_vx = 0
+	if (idx in (6,5,4)):
+		_vx = -1
+	return (_vx, _vy)
+
+# Return true if inside, false if outside
+def is_inside_img_bounds(_x, _y):
+	return not ((_x < 0) or (_x > img_x-1) or (_y < 0) or (_y > img_y-1))
+
+# Check for position bounds and adjust velocity
+def adjust_for_img_bounds(_x, _y, _vx, _vy, particle):
+	if (_x + _vx < 0):   # check min bounds x
+		_x = 0
+		_vx *= -1
+		particle['in_color_count'] = 0   # reset counter
+	elif (_x + _vx > (img_x - 1)):   # check max bounds x, sub 1 bc img_x is size not index
+		_x = (img_x - 1)
+		_vx *= -1
+		particle['in_color_count'] = 0   # reset counter
+	else:
+		_x += _vx   # normal movement
+
+	if (_y + _vy < 0):   # check min bounds y
+		_y = 0
+		_vy *= -1
+		particle['in_color_count'] = 0   # reset counter
+	elif (_y + _vy > (img_y - 1)):   # check max bounds y
+		_y = (img_y - 1)
+		_vy *= -1
+		particle['in_color_count'] = 0   # reset counter
+	else:
+		_y += _vy   # normal movement
+	return _x, _y, _vx, _vy
 
 def update(frame_number):
+	#print "Frame %d" % frame_number
+
 	for p_idx,particle in enumerate(swarm):
 		x = particle['position'][0]   # position x coordinate
 		y = particle['position'][1]   # position y coordinate
 		vx = particle['velocity'][0]   # velocity x component
 		vy = particle['velocity'][1]   # velocity y component
-		
-		# Check for position bounds and adjust velocity
-		if (x + vx < 0):   # check min bounds x
-			x = 0
-			vx *= -1
-		elif (x + vx > (img_x - 1)):   # check max bounds x, sub 1 bc img_x is size not index
-			x = (img_x - 1)
-			vx *= -1
-		else:
-			x += vx   # normal movement
 
-		if (y + vy < 0):   # check min bounds y
-			y = 0
-			vy *= -1
-		elif (y + vy > (img_y - 1)):   # check max bounds y
-			y = (img_y - 1)
-			vy *= -1
-		else:
-			y += vy   # normal movement
-
-		# Check position and surrounding color data
+		# Check position and neighbor color data
 		position_color = tuple(img[y][x])
 		#print "P%d color = %s" % (idx, position_color)
-		surrounding_colors = [None,None,None,None,None,None,None,None]
-		try:
-			# check bounds for using [y|x][minus|plus]
-			xm = x != 0   # can use x-1
-			ym = y != 0   # can use y-1
-			xp = x != img_x-1   # can use x+1
-			yp = y != img_y-1   # can use y+1
 
-			# 0 1 2
-			# 3 X 4
-			# 5 6 7
+		# Check that state is travelling
+		if (particle['state'] == 0):
+			# Check if current position is in color
+			if (is_in_color(position_color)):
+				print "X(%d, %d), V(%d, %d), RED, CC=%d : in red" % (x,y,vx,vy, particle['in_color_count'])
 
-			if (yp):
-				surrounding_colors[1] = tuple(img[y+1][x])
-				if (xm):
-					surrounding_colors[0] = tuple(img[y+1][x-1])
-				if (xp):
-					surrounding_colors[2] = tuple(img[y+1][x+1])
-			if (xm):
-				surrounding_colors[3] = tuple(img[y][x-1])
-			if (xp):
-				surrounding_colors[4] = tuple(img[y][x+1])
-			if (ym):
-				surrounding_colors[6] = tuple(img[y-1][x])
-				if (xm):
-					surrounding_colors[5] = tuple(img[y-1][x-1])
-				if (xp):
-					surrounding_colors[7] = tuple(img[y-1][x+1])
-		except:
-			print "error"
+				particle['in_color_count'] += abs(vx) + abs(vy)   # increment counter
 
-		# Check if position color in color threshold
-		if (is_in_color(position_color)):
-			print position_color,"IS RED!"
-
-			# Find direction of best red
-			best_node_idx = -1
-			best_dist = 1000
-			for c_idx,surrounding_color in enumerate(surrounding_colors):
-				dist = abs(search_color[0] - surrounding_color[0]) + \
-					abs(search_color[1] - surrounding_color[1]) + abs(search_color[2] - surrounding_color[2])
-				if (dist < best_dist):
-					best_dist = dist
-					best_node_idx = c_idx
-
-			print "\tMoving to idx:",best_node_idx
-
-			if (particle['in_color_count'] == 0):
-				# Change velocity wrt current velocity
-				if (best_node_idx <= 2):
-					vy = 1
-				elif (best_node_idx <= 4):
-					vy = 0
-				else:
-					vy = -1
-
-				if (best_node_idx in (2,4,7)):
-					vx = 1
-				elif (best_node_idx in (1,6)):
-					vx = 0
-				else:
-					vx = -1
-
-			particle['in_color_count'] += 1
-		else:
-			print position_color
-
-			if (particle['in_color_count'] > 0):
-				# particle was just in color, reverse
-				vx *= -1
-				vy *= -1
+				# Check if travelled far enough into blob
+				if (particle['in_color_count'] >= min_blob_size):
+					particle['state'] = 1
+					print "\tEntered in blob state"
 			else:
-				particle['in_color_count'] = 0
-				# Randomly change velocity
+				print position_color, 
+				print "X(%d, %d), V(%d, %d) : not in color" % (x,y,vx,vy)
+				
+				particle['in_color_count'] = 0   # reset counter
+
+				# Randomly mutate velocity
 				vx = vx if (np.random.rand() > 0.02) else get_rand_velocity(1, 1) 
 				vy = vy if (np.random.rand() > 0.02) else get_rand_velocity(1, 1)
-			
 
+			x, y, vx, vy = adjust_for_img_bounds(x, y, vx, vy, particle)
+
+		# Check that state is searching blob
+		if (particle['state'] == 1):
+			print "X(%d, %d), V(%d, %d), %s : State == 1" % (x,y,vx,vy,"RED" if is_in_color(position_color) else "",)
+
+			particle['in_color_count'] += abs(vx) + abs(vy)   # increment counter
+
+			"""
+			while inside valid blob
+			travel until edge but don't overshoot
+			once on edge, stop/store radius counter
+
+			"""
+			if (is_inside_img_bounds(x + vx, y + vy) and not is_in_color(tuple(img[y + vy][x + vx]))):
+				# Don't overshoot boundary, but don't reverse
+				# need to reduce velocity to just hit in color edge
+							
+				next_vx = vx * -1   # store reversed velocity
+				next_vy = vy * -1   # store reversed velocity
+				
+				safety = 20
+				while (True):
+					safety -= 1
+					if (safety == 0):
+						print "ERROR IN LOOP!"
+						break
+					if (vx != 0):
+						vx += -1 if vx > 0 else 1
+					if (is_in_color(tuple(img[y + vy][x + vx]))):
+						break
+					if (vy != 0):
+						vy += -1 if vy > 0 else 1
+					if (is_in_color(tuple(img[y + vy][x + vx]))):
+						break
+				x = x + vx
+				y = y + vy
+				vx = next_vx
+				vy = next_vy
+				print "\tflipped V, setting x=%d,y=%d" % (x,y)
+			else:
+				print "\tnormal update"
+				x, y, vx, vy = adjust_for_img_bounds(x, y, vx, vy, particle)
 
 		# set particle data
 		particle['position'][0] = x
@@ -200,8 +228,9 @@ def update(frame_number):
 	set_markers()   # update rotation of plot markers
 
     # Update the scatter collection
-	scatter_plot.set_offsets(swarm['position'])
 	scatter_plot.set_facecolor(swarm['color'] / 255.0)
+	scatter_plot.set_offsets(swarm['position'])
+	
 
 	#if (frame_number == 20):
 	#	animation.event_source.stop()
@@ -210,7 +239,12 @@ def update(frame_number):
 def onclick(event):
 	try:
 		# Print image data at click location
-		print "click at (%.2f, %.2f) : %s" % (event.xdata, event.ydata, tuple(img[int(event.ydata)][int(event.xdata)]))
+		if (event.ydata is not None and event.xdata is not None):
+			x = int(round(event.xdata))
+			y = int(round(event.ydata))
+			color = tuple(img[y][x])
+			print "click at (%d, %d) : %s : %s" \
+				% (x,y, color, "IN_COLOR" if is_in_color(color) else "NOT_IN_COLOR")
 	except Exception as e:
 		print e
 		pass
@@ -239,14 +273,16 @@ def onpress(event):
 		animation.event_source.interval = interval
 
 run_anim = True
-interval = 200
+interval = 500
 min_interval = 25
 max_interval = 500
+iterations = 180
 click_cid = fig.canvas.mpl_connect('button_press_event', onclick)
 btn_cid = fig.canvas.mpl_connect('key_press_event', onpress)
 
 plt.grid(True)   # Show grid on axes
-animation = FuncAnimation(fig, update, interval=interval)
+animation = FuncAnimation(fig, update, interval=interval)#, frames=iterations)
 plt.show()   # Show plot
+#animation.save("path1.mp4", fps=20)
 
 
